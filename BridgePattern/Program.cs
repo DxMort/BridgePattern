ce39b89a8082bde0b24fa1c363851370b0ffdf85
DxMort <99000786+DxMort@users.noreply.github.com>
2024-10-21 10:16:22 +0700

    Add files via upload

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using BridgePattern;
+
+class Program
+{
+    static void Main()
+    {
+        INotification email = new EmailNotification();
+        INotification sms = new SmsNotification();
+
+        Notify emailNotification1 = new TextNotify(email);
+        emailNotification1.SendNotify("Драйв — не вкусно, лучше бери берн", "karpenkoev@mer.ci.nsu.ru");
+
+        Notify emailNotification2 = new HTMLNotify(email);
+        emailNotification2.SendNotify("В уборную надо - купи торнадо!", "knigamy@mer.ci.nsu.ru");
+
+        Notify smsNotification1 = new TextNotify(sms);
+        smsNotification1.SendNotify("Берн с гуавой - имба", "+7 (996) 606 96 09 ");
+
+        Notify smsNotification2 = new HTMLNotify(sms);
+        smsNotification2.SendNotify("Флеш с карамболой - имба", "+7 (999) 909 90 09");
+    }
+}
